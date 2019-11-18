dcef25781c169fc29b6ee818c949e1033df9a92a
Shouly <shoulyliang@gmail.com>
2019-11-18 14:39:55 +0800

    add murmur hash

---
+++ src/main/java/me/shouly/hash/MurmurHash.java
@@ -0,0 +1,15 @@
+package me.shouly.hash;
+
+/**
+ * MurmurHash算法
+ * <a>https://zh.wikipedia.org/wiki/Murmur%E5%93%88%E5%B8%8C</a>
+ *
+ * @author liangbing
+ * @version v1.0
+ * @date 2019/11/16 3:57 下午
+ **/
+public class MurmurHash {
+
+
+
+}
